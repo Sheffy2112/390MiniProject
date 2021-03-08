b59161d3711dfa6c491583950f3355e5a96e698f
Drew Anderson <50057794+drewanderson17@users.noreply.github.com>
2021-03-08 12:32:56 -0500

    Added the testing functionality under function results and BleuScore

---
+++ Perplexity.py
@@ -0,0 +1,59 @@
+
+
+from nltk.tokenize import RegexpTokenizer
+
+import collections, nltk
+
+from nltk.tokenize import RegexpTokenizer
+from keras.models import Sequential, load_model
+# we first tokenize the text corpus
+import numpy as np
+corpus ="""
+Monty Python (sometimes known as The Pythons) were a British surreal comedy group who created the sketch comedy show Monty Python's Flying Circus,
+that first aired on the BBC on October 5, 1969. Forty-five episodes were made over four series. The Python phenomenon developed from the television series
+into something larger in scope and impact, spawning touring stage shows, films, numerous albums, several books, and a stage musical.
+The group's influence on comedy has been compared to The Beatles' influence on music."""
+
+path = "C:/Users/drewa/OneDrive/Documents/GitHub/390MiniProject/1661-0.txt"
+
+text = open(path, encoding='utf-8').read().lower()
+
+
+tokenizer = RegexpTokenizer(r'\w+')
+words = tokenizer.tokenize(text)
+
+
+
+model = load_model('keras_next_word_model.h5')
+#here you construct the unigram language model
+def unigram(tokens):
+    model = collections.defaultdict(lambda: 0.01)
+    for f in tokens:
+        try:
+            model[f] += 1
+        except KeyError:
+            model [f] = 1
+            continue
+    N = float(sum(model.values()))
+    for word in model:
+        model[word] = model[word]/N
+    return model
+
+
+#computes perplexity of the unigram model on a testset
+def perplexity(testset, model):
+    testset = testset.split()
+    perplexity = 1
+    N = 0
+    for word in testset:
+        N += 1
+        perplexity = perplexity * (1/model[word])
+    perplexity = pow(perplexity, 1/float(N))
+    return perplexity
+
+testset1 = "Sherlock Holmes"
+testset2 = "abracadabra gobbledygook rubbish"
+
+model = unigram(words)
+print(perplexity(testset1, model))
+print(perplexity(testset2, model))
